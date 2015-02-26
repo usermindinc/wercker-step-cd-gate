724adb1b27e919177392e5a8884bc4c8c2e91735
Chris Sullins <csullins@usermind.com>
2015-02-25 16:32:00 -0800

    Add aliases for production and staging.

--- run.sh
+++ run.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+# Alias environment
+case "$WERCKER_CD_GATE_ENVIRONMENT" in
+  "p" | "prod" | "prodsky")
+    WERCKER_CD_GATE_ENVIRONMENT=production
+    ;;
+  "s" | "stag" | "stage" | "boom")
+    WERCKER_CD_GATE_ENVIRONMENT=staging
+    ;;
+esac
+
 set -x
 ENVIRONMENT_FLAG_URL="$WERCKER_CD_GATE_ENDPOINT/$WERCKER_CD_GATE_ENVIRONMENT"
 APPLICATION_FLAG_URL="$ENVIRONMENT_FLAG_URL/$WERCKER_CD_GATE_APPLICATION"
